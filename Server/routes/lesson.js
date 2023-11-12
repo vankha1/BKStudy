e9f3e1077db38bec66615687acf58d2f46af4903
Fooc <nglephuc03@gmail.com>
2023-11-12 10:52:46 +0700

    change comment //

--- Server/routes/lesson.js
+++ Server/routes/lesson.js
@@ -10,15 +10,15 @@ const isAuth = require('../middleware/isAuth');
 
 // user get all lesson from the course
 router.get('/course/:courseId/',
-    //isAuth.authToken,
-    //isAuth.authRoles(["LECTURER", "STUDENT"]),
+    isAuth.authToken,
+    isAuth.authRoles(["LECTURER", "STUDENT"]),
     lessonController.getAllLessons
 );
 
 // user get a specific lesson from the course
 router.get('/course/:courseId/lesson/:lessonId',
-    //isAuth.authToken,
-    //isAuth.authRoles(["LECTURER", "STUDENT"]),
+    isAuth.authToken,
+    isAuth.authRoles(["LECTURER", "STUDENT"]),
     lessonController.getLesson
 )
 
@@ -27,36 +27,38 @@ router.get('/course/:courseId/lesson/:lessonId',
 
 // teacher create a lesson in the course
 router.post('/course/:courseId/create-lesson',
-    //isAuth.authToken,
-    //isAuth.authRoles(["LECTURER"]),
+    isAuth.authToken,
+    isAuth.authRoles(["LECTURER"]),
     lessonFileMulter.uploadLessonFile.array("files"),
     lessonController.createLesson
 );
 
 // teacher update lesson's information in the course
 router.put('/course/:courseId/lesson/:lessonId/update-lesson',
-    //isAuth.authToken,
-    //isAuth.authRoles(["LECTURER"]),
+    isAuth.authToken,
+    isAuth.authRoles(["LECTURER"]),
     lessonFileMulter.uploadLessonFile.array("files"),
     lessonController.updateLesson
 );
 
 // teacher delete lesson in the course
 router.delete('/course/:courseId/lesson/:lessonId/delete-lesson',
-    //isAuth.authToken,
-    //isAuth.authRoles(["LECTURER"]),
+    isAuth.authToken,
+    isAuth.authRoles(["LECTURER"]),
     lessonController.deleteLesson
 );
 
 // student's priviledge
 
 router.put('/course/:courseId/lesson/:lessonId/update-note',
-    //isAuth.authToken,
-    //isAuth.authRoles(["STUDENT"]),
+    isAuth.authToken,
+    isAuth.authRoles(["STUDENT"]),
     lessonController.updateNote
 );
 
 router.get('/download/:filepath',
+    isAut.authToken,
+    isAuth.authRoles(["STUDENT"]),
     lessonController.downloadFile
 );
 
