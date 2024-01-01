1b1e34d6a9cfca957276d4c709c7a1cd62d56ab1
havanchau <chauhvc2.dy@gmail.com>
2024-01-01 13:07:57 +0700

    update: update css for payment and success

--- Client/src/app/success/page.jsx
+++ Client/src/app/success/page.jsx
@@ -1,8 +1,11 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { useSearchParams , useRouter } from "next/navigation";
 import axios from "axios";
+import Image from "next/image";
+import React, { useEffect } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
+import LoadingState from "@components/LoadingState";
 
 const Success = () => {
   const router = useRouter();
@@ -37,9 +40,24 @@ const Success = () => {
   }, []);
 
   return (
-    <div>
-      Payment successful. You are being redirected to the orders page. Please do
-      not close the page
+    <div className="w-full">
+      <div className="w-[25rem] mx-auto my-24">
+        <Image
+          className=""
+          src='/assets/images/successful_image.jpg'
+          alt="Successful Picture"
+          width={400}
+          height={280}
+          priority
+        />
+        <div className="flex-between">
+          <LoadingState width={60} height={60}/>
+          <div className="text-xl">
+            Payment successful. You are being redirected to the orders page. Please do
+            not close the page.
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
